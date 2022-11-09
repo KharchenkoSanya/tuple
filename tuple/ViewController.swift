708874f38595c53eeb0285fdccdf263067f4e67f
KharchenkoSanya <kharchenko1996oleksandr@gmail.com>
2022-11-09 18:06:58 +0100

    First commit

---
+++ tuple/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  tuple
+//
+//  Created by Sashka IOS Developer on 09.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+var a = 1
+var b = 2
+            
+print(a)
+print(b)
+        
+(a,b) = (b,a)
+  
+print(a)
+print(b)
+        
+    }
+}
+
